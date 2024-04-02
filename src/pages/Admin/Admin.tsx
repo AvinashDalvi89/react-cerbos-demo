a8d923eec1114a262667bf2e8691be9de1940f49
Avinash Dalvi <avinashsdalvi@gmail.com>
2024-04-02 15:40:10 +0530

    added role permision to admin page

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,47 +1,70 @@
-import React, { FC } from 'react'; 
+import React, { FC, useState } from 'react'; 
 import { getCerbosClient } from '../../lib/Cerbos';
 import { useEffect } from 'react';
 interface UserDetails {
     username: string;
+    role: string,
+    password: string
   }
 const Admin: FC = () => {
     let userDetails: UserDetails | null = JSON.parse(localStorage.getItem('userDetails') || '');
-
+    const [data, setData] = useState(Boolean);
+    
+    async function checkAccess() {
+        const cerbos = getCerbosClient();
+        let role; 
+        if(userDetails === null){
+            role = "Demo";
+        }
+        else{
+            role = userDetails.role;
+        }
+        const contactQueryPlan = await cerbos.isAllowed({
+            principal: {
+            id: "demoUserID",
+            roles: [role],
+            attr: { },
+            },
+            resource: {
+            kind: "admin",
+            id: "1",
+            },
+            action: "read",
+        }); // => true
+        console.log(contactQueryPlan); 
+        setData(contactQueryPlan);
+        return contactQueryPlan;
+        
+    }
     useEffect(() => {
-        checkAccess(userDetails);
-     }, []); // Pass an empty array to only call the function once on mount.
-
-  return (
-    <> 
-      <div className='container'>
-        <h2>Admin</h2>
-      </div>
-    </>
-  );
+       checkAccess();
+     }, [userDetails]); // Pass an empty array to only call the function once on mount.
+ 
+     if (data) {
+        return (
+            <> 
+              <div className='container'>
+                <h2>Admin Access</h2>
+                
+              </div>
+            </>
+          );
+     }
+     else{
+        return (
+            <> 
+              <div className='container'>
+                <h2>Sorry don't have access to this page</h2>
+                
+              </div>
+            </>
+          );
+     }
+ 
 }
 
-export async function checkAccess(userDetails: UserDetails | null) {
-    const cerbos = getCerbosClient();
-    let role;
-    if(userDetails === null){
-        role = "Demo";
-    }
-    else{
-        role = "afafs";
-    }
-    const contactQueryPlan = await cerbos.isAllowed({
-        principal: {
-          id: "demoUserID",
-          roles: ['admin'],
-          attr: { },
-        },
-        resource: {
-          kind: "admin",
-          id: "1",
-        },
-        action: "read",
-      }); // => true
-     console.log(contactQueryPlan);
-}
 
 export default Admin;
+ 
+ 
+
