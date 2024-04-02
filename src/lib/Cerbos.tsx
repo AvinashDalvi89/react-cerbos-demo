1e82c89a2643440cf3a225958b1759812a7912e2
Avinash Dalvi <avinashsdalvi@gmail.com>
2024-04-02 22:31:57 +0530

    Added playground demo link code

--- src/lib/Cerbos.tsx
+++ src/lib/Cerbos.tsx
@@ -2,8 +2,19 @@
 import { HTTP } from "@cerbos/http";
 
 export function getCerbosClient(){
-    const cerbos = new HTTP("http://localhost:3592");
-    return cerbos;
+    let usePlayground = false;
+    if(usePlayground){
+        const cerbos = new HTTP("https://demo-pdp.cerbos.cloud", {
+            playgroundInstance: "LnQCrZuCzZ660bda75BB9iF2qQdLutza", // The playground instance ID to test
+       });
+       return cerbos;
+    }
+    else{
+        const cerbos = new HTTP("http://localhost:3592");
+        return cerbos;
+    }
+   
+    
 }
 
 
